7148d5a091f675deb86ff42c26d59e7a4950a5b3
Danial <Danialsb778@gmail.com>
2022-10-21 09:47:50 +0330

    uploading the files

---
+++ backend/sql_connection.py
@@ -0,0 +1,13 @@
+import datetime
+import mysql.connector
+
+__mydb = None
+
+def get_sql_connection():
+  print("Opening mysql connection")
+  global __mydb
+
+  if __mydb is None:
+    __mydb = mysql.connector.connect(user='root', password='ds20011379', database='grocery_store')
+
+  return __mydb
